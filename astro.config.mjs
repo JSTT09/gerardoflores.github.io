8456b02cda52ea7d8f93d4b7f4c9df2bdeebe9b7
JST <gerardo.flores.c03@gmail.com>
2025-09-07 22:13:51 -0600

    Initial commit - CV Harvard style

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({});
+
+const LIVE_URL = "https://gerardoflores.github.io";
+
+
+
