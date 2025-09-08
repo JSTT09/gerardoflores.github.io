2757215f212f28763c167eeadb790d669c0197b6
JST <gerardo.flores.c03@gmail.com>
2025-09-07 22:24:32 -0600

    Add GitHub Pages configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
-
-const LIVE_URL = "https://gerardoflores.github.io";
+export default defineConfig({
+  site: 'https://jstt09.github.io',
+  base: '/gerardoflores.github.io',
+  build: {
+    assets: 'astro'
+  }
+});
 
 
 
